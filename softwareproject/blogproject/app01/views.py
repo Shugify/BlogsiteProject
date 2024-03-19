17b2dae7a690f7bb7305e7c2bb7c7ce23ba75d0d
yyl1105 <98073537+yyl1105@users.noreply.github.com>
2024-03-19 14:52:19 +0800

    Update views.py

--- softwareproject/blogproject/app01/views.py
+++ softwareproject/blogproject/app01/views.py
@@ -1,5 +1,32 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("这里是liujiangblog.com的投票站点")
+    pass
+    return render(request, 'app01/index.html')
+
+
+def login(request):
+    pass
+    return render(request, 'app01/login.html')
+
+
+def my_article(request):
+    pass
+    return render(request, 'app01/my_article.html')
+
+
+def article(request):
+    pass
+    return render(request, 'app01/article.html')
+
+
+def account_setting(request):
+    pass
+    return render(request, 'app01/account_setting.html')
+
+
+def category(request):
+    pass
+    return render(request, 'app01/category.html')
