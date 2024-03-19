afa24c208ddac35fa212686eb24a5ba0909da8d9
yyl1105 <98073537+yyl1105@users.noreply.github.com>
2024-03-19 14:51:53 +0800

    Update urls.py

--- softwareproject/blogproject/app01/urls.py
+++ softwareproject/blogproject/app01/urls.py
@@ -1,5 +1,18 @@
 from django.urls import path
 from . import views
 urlpatterns = [
+    # 首页
     path('', views.index, name='index'),
+    # 登录页
+    path('login/', views.login, name='login'),
+    # 主页面
+    path('index/', views.index, name='index'),
+    # 个人博客页
+    path('my_article/', views.my_article, name='my_article'),
+    # 文章详情页
+    path('article/', views.article, name='article'),
+    # 个人信息页
+    path('account_setting/', views.account_setting, name='account_setting'),
+    # 分类页
+    path('category/', views.category, name='category'),
 ]
