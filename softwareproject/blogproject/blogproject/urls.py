d330206d5e8c88f0c1b48d984abd83f1058d4a14
xsx-123 <92916482+xsx-123@users.noreply.github.com>
2024-04-08 16:48:00 +0800

    Add files via upload

--- softwareproject/blogproject/blogproject/urls.py
+++ softwareproject/blogproject/blogproject/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from app01 import views
+
 urlpatterns = [
     path('', include('app01.urls')),
     path('admin/', admin.site.urls),
+    path('article/<int:id>/', views.article_detail, name='article'),  # 文章详情
 ]
