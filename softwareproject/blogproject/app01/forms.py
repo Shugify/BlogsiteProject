133de3e9112e00a6f02a8c522ce1032fa633b1f0
Shugify <92961237+Shugify@users.noreply.github.com>
2024-04-29 17:40:31 +0800

    Update forms.py

--- softwareproject/blogproject/app01/forms.py
+++ softwareproject/blogproject/app01/forms.py
@@ -55,3 +55,34 @@ class UserChangePwdForm(forms.Form):
     oldPassword = forms.CharField(widget=forms.PasswordInput, label='原密码')
     newPassword = forms.CharField(widget=forms.PasswordInput, label='新密码')
     checkPassword = forms.CharField(widget=forms.PasswordInput, label='确认密码')
+
+
+
+
+class ADLoginForm(forms.Form):
+    aid = forms.CharField(label='AD ID', max_length=100, required=True)
+    pwd = forms.CharField(label='Password', max_length=100, widget=forms.PasswordInput, required=True)
+
+class AdministratorChangePwdForm(forms.Form):
+    oldPassword = forms.CharField(widget=forms.PasswordInput, label='原密码')
+    newPassword = forms.CharField(widget=forms.PasswordInput, label='新密码')
+    checkPassword = forms.CharField(widget=forms.PasswordInput, label='确认密码')
+
+
+class AdministratorRegistrationForm(forms.Form):
+    name = forms.CharField(max_length=100, label='姓名')
+    email = forms.EmailField(label='邮箱')
+    phone = forms.CharField(max_length=20, label='电话')
+    password = forms.CharField(widget=forms.PasswordInput, label='密码')
+    rpassword = forms.CharField(widget=forms.PasswordInput, label='确认密码')
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        rpassword = cleaned_data.get('rpassword')
+
+        # 确认密码匹配验证
+        if password and rpassword and password != rpassword:
+            raise forms.ValidationError("密码不匹配，请重新输入。")
+
+        return cleaned_data
